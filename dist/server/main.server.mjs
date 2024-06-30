53703f13d58db9a72f7d3c9af0e89199acc4ac44
nayyars <nayyar.it@gmail.com>
2024-06-30 22:10:35 +0530

    Include dist folder

---
+++ dist/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-SHI7VTPG.mjs";import"./chunk-OYRDBF33.mjs";import"./chunk-QRWQY24P.mjs";import"./chunk-RYOYOGOK.mjs";import"./chunk-MUB4C7OA.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
